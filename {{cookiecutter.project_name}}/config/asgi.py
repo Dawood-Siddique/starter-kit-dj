9d67ad1eb1549e52267c211a687b8ecbceb15399
Dawood-Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2026-03-22 20:26:27 +0500

    Update asgi.py

--- {{cookiecutter.project_name}}/config/asgi.py
+++ {{cookiecutter.project_name}}/config/asgi.py
@@ -4,13 +4,29 @@ ASGI config for config project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
-application = get_asgi_application()
+# Import routing and middleware after django.setup()
+from apps.notifications.middleware import TokenAuthMiddleware
+import apps.notifications.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": TokenAuthMiddleware(
+            AuthMiddlewareStack(
+                URLRouter(apps.notifications.routing.websocket_urlpatterns)
+            )
+        ),
+    }
+)
